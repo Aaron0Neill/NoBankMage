4ae69ad9542be18a0f536a9c5866a3ada94cc0a4
ben-millar <c00236772@itcarlow.ie>
2023-09-26 19:18:51 +0100

    Initial commit of starter project

---
+++ ECS Library/Globals.h
@@ -0,0 +1,10 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <iostream>
+
+#define WINDOW_HEIGHT sf::VideoMode::getDesktopMode().height / 4 * 3
+#define WINDOW_WIDTH WINDOW_HEIGHT
+#define RESOLUTION sf::Vector2f{ 1080.f, 1080.f }
+
+#endif
